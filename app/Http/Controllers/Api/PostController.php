495991982c9aed50bd02b575cb4ebcf3b8d11144
musta <mustafa774519161@gmail.com>
2024-11-27 23:08:14 +0300

    fef

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -17,38 +17,38 @@ class PostController extends Controller
         // return Post::all();
     }
 
-    public function store(Request $request)
-    {
-        // $post = Post::create($request->validate([
-        //     'title' => 'required|string|max:255',
-        //     'content' => 'required|string',
-        // ]));
-        // return response()->json($post, 201);
-        return new JsonResponse([
-            'data' => 11111
-        ]);
-    }
+    // public function store(Request $request)
+    // {
+    //     // $post = Post::create($request->validate([
+    //     //     'title' => 'required|string|max:255',
+    //     //     'content' => 'required|string',
+    //     // ]));
+    //     // return response()->json($post, 201);
+    //     return new JsonResponse([
+    //         'data' => 11111
+    //     ]);
+    // }
 
-    public function show(Post $post)
-    {
-        return new JsonResponse([
-            'data' => 55555
-        ]);
-        // return $post;
-    }
+    // public function show(Post $post)
+    // {
+    //     return new JsonResponse([
+    //         'data' => 55555
+    //     ]);
+    //     // return $post;
+    // }
 
-    public function update(Request $request, Post $post)
-    {
-        $post->update($request->validate([
-            'title' => 'sometimes|required|string|max:255',
-            'content' => 'sometimes|required|string',
-        ]));
-        return response()->json($post);
-    }
+    // public function update(Request $request, Post $post)
+    // {
+    //     $post->update($request->validate([
+    //         'title' => 'sometimes|required|string|max:255',
+    //         'content' => 'sometimes|required|string',
+    //     ]));
+    //     return response()->json($post);
+    // }
 
-    public function destroy(Post $post)
-    {
-        $post->delete();
-        return response()->json(null, 204);
-    }
+    // public function destroy(Post $post)
+    // {
+    //     $post->delete();
+    //     return response()->json(null, 204);
+    // }
 }
