9d1b09a9ed4454a014b32f30a6dcf82826dddb74
musta <mustafa774519161@gmail.com>
2024-11-27 23:09:30 +0300

    feg

--- routes/api.php
+++ routes/api.php
@@ -24,9 +24,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('posts', PostController::class);
+
 
 Route::prefix('v1')->group(function() {
+    Route::apiResource('posts', PostController::class);
     // Route::apiResource('users', UserController::class);
     // Route::apiResource('posts', PostController::class);
     // Route::apiResource('comments', CommentController::class);
