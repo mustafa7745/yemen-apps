2f6257fa41d691ec3ef2b09069ed826551aa66d6
musta <mustafa774519161@gmail.com>
2024-12-03 20:40:05 +0300

    sds

--- app/Http/Controllers/Api/StoreManager/StoreManagerController.php
+++ app/Http/Controllers/Api/StoreManager/StoreManagerController.php
@@ -672,7 +672,7 @@ class StoreManagerController extends Controller
         $product = DB::table(table: Products::$tableName)->where(Products::$tableName . '.' . Products::$id, '=', $insertedId)
             ->first(
             );
-        $product['images'] = [];
+        $product->images = [];
 
         return response()->json($product);
     }
