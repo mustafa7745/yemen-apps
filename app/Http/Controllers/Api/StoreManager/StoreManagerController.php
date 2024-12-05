4b4ed664813e8fa8a41038d83fce4c1f5fa1c5c8
musta <mustafa774519161@gmail.com>
2024-12-05 20:34:31 +0300

    sdf

--- app/Http/Controllers/Api/StoreManager/StoreManagerController.php
+++ app/Http/Controllers/Api/StoreManager/StoreManagerController.php
@@ -30,6 +30,12 @@ class StoreManagerController extends Controller
     private $appId = 1;
     public function readMain(Request $request)
     {
+        $request->validate([
+            'name' => 'required',  // Makes the 'name' field required
+            'email' => 'required|email',  // Makes the 'email' field required and must be a valid email
+            'password' => 'required|min:6',  // Makes the 'password' field required and at least 6 characters long
+        ]);
+
         $loginController = (new LoginController($this->appId));
         $token = $request->input('accessToken');
         $deviceId = $request->input('deviceId');
