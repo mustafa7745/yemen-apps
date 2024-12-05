450a576606929ea89288c27fd532090a8a3b8991
musta <mustafa774519161@gmail.com>
2024-12-05 20:51:30 +0300

    dsd

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -275,8 +275,9 @@ class LoginController
             ]);
         // print_r($accessToken->toSql());
         if ($accessToken == null) {
-            return new MyResponse(false, "Inv Tok", 404, 20005);
+            return new MyResponse(false, "Inv Tok", 403, 20005);
         }
+        print_r($accessToken);
         return new MyResponse(true, $accessToken, 200, 20005);
         // return $accessToken;
     }
