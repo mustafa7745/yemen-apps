251e4ba9bfd58d5a3068d6a93acfc8dffbc1ccd3
musta <mustafa774519161@gmail.com>
2025-01-01 17:18:24 +0300

    dfe

--- app/Http/Controllers/Api/Users/UserControllerGet.php
+++ app/Http/Controllers/Api/Users/UserControllerGet.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Users;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Controller;
 use App\Models\AppStores;
+use App\Models\SharedStoresConfigs;
 use App\Models\Stores;
 use App\Traits\UsersControllerShared;
 use Illuminate\Support\Facades\DB;
@@ -47,14 +48,35 @@ class UserControllerGet extends Controller
                 Stores::$tableName . '.' . Stores::$subscriptions,
                 Stores::$tableName . '.' . Stores::$stars,
                 Stores::$tableName . '.' . Stores::$reviews,
-
-
-                // Stores::$tableName . '.' . Stores::$,
-                // Stores::$tableName . '.' . Stores::$id,
-                // Stores::$tableName . '.' . Stores::$id,
-                // Stores::$tableName . '.' . Stores::$id,
-                // Stores::$tableName . '.' . Stores::$id,
             ]);
+
+        $storeIds = [];
+        foreach ($data as $store) {
+            if ($store->typeId == 1) {
+                $storeIds[] = $store->id;
+            }
+        }
+
+        $storeConfigs = DB::table(table: SharedStoresConfigs::$tableName)
+            ->whereIn(SharedStoresConfigs::$tableName . '.' . SharedStoresConfigs::$storeId, $storeIds)
+            ->get();
+
+
+        foreach ($storeConfigs as $storeConfig) {
+            foreach ($data as $index => $store) {
+                // print_r($storeConfig);
+                if ($storeConfig->storeId == $store->id && $store->typeId == 1) {
+                    $categories = json_decode($storeConfig->categories);
+                    $sections = json_decode($storeConfig->sections);
+                    $nestedSections = json_decode($storeConfig->nestedSections);
+                    $products = json_decode($storeConfig->products);
+                    // $stores[$index] = (array)$stores[$index];
+                    $data[$index]->storeConfig = ['storeIdReference' => $storeConfig->storeIdReference, 'categories' => $categories, 'sections' => $sections, 'nestedSections' => $nestedSections, 'products' => $products];
+                } else
+                    $data[$index]->storeConfig = null;
+            }
+        }
+
         return $data;
     }
 }
