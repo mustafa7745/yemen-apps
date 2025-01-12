19a519cb6e713f861b0daa1c580e004fc7f7bd6f
musta <mustafa774519161@gmail.com>
2025-01-12 21:18:40 +0300

    dfef

--- app/Http/Controllers/Api/Delivery/DeliveryControllerGet.php
+++ app/Http/Controllers/Api/Delivery/DeliveryControllerGet.php
@@ -20,51 +20,54 @@ class DeliveryControllerGet extends Controller
 
     public function login(Request $request)
     {
-        $loginController = (new LoginController($this->appId));
-        $res = $loginController->loginNew($request);
-        if ($res == false) {
-            return response()->json(["message" => $res->message, 'code' => $res->code, 'errors' => []], $res->responseCode);
-        }
+        return DB::transaction(function () {
+            $loginController = (new LoginController($this->appId));
+            $res = $loginController->loginNew($request);
+            if ($res == false) {
+                return response()->json(["message" => $res->message, 'code' => $res->code, 'errors' => []], $res->responseCode);
+            }
 
 
-        // print_r($res->message['token']);
-        $token = $res->message['token'];
-        $userId = DB::table(table: AccessTokens1::$tableName)
-            ->join(
-                UsersSessions::$tableName,
-                UsersSessions::$tableName . '.' . UsersSessions::$id,
-                '=',
-                AccessTokens1::$tableName . '.' . AccessTokens1::$userSessionId
-            )
-            ->where(AccessTokens1::$tableName . '.' . AccessTokens1::$token, '=', $token)
-            ->first(
-                [
-                    UsersSessions::$tableName . '.' . UsersSessions::$userId
-                ]
-            );
+            // print_r($res->message['token']);
+            $token = $res->message['token'];
+            $userId = DB::table(table: AccessTokens1::$tableName)
+                ->join(
+                    UsersSessions::$tableName,
+                    UsersSessions::$tableName . '.' . UsersSessions::$id,
+                    '=',
+                    AccessTokens1::$tableName . '.' . AccessTokens1::$userSessionId
+                )
+                ->where(AccessTokens1::$tableName . '.' . AccessTokens1::$token, '=', $token)
+                ->first(
+                    [
+                        UsersSessions::$tableName . '.' . UsersSessions::$userId
+                    ]
+                );
 
-        // $userId = $data->userId;
+            // $userId = $data->userId;
 
 
 
-        $count = DB::table(table: DeliveryMen::$tableName)
-            ->where(DeliveryMen::$tableName . '.' . DeliveryMen::$userId, '=', $userId)
-            ->count();
+            $count = DB::table(table: DeliveryMen::$tableName)
+                ->where(DeliveryMen::$tableName . '.' . DeliveryMen::$userId, '=', $userId)
+                ->count();
 
-        if ($count == 0) {
-            $inserted = DB::table(table: DeliveryMen::$tableName)
-                ->insert([
-                    DeliveryMen::$id => null,
-                    DeliveryMen::$userId => $userId,
-                    DeliveryMen::$createdAt => Carbon::now()->format('Y-m-d H:i:s'),
-                    DeliveryMen::$updatedAt => Carbon::now()->format('Y-m-d H:i:s'),
-                ]);
-            if ($inserted) {
-                return response()->json($res->message);
+            if ($count == 0) {
+                $inserted = DB::table(table: DeliveryMen::$tableName)
+                    ->insert([
+                        DeliveryMen::$id => null,
+                        DeliveryMen::$userId => $userId,
+                        DeliveryMen::$createdAt => Carbon::now()->format('Y-m-d H:i:s'),
+                        DeliveryMen::$updatedAt => Carbon::now()->format('Y-m-d H:i:s'),
+                    ]);
+                if ($inserted) {
+                    return response()->json($res->message);
+                } else
+                    return response()->json(["message" => "Error on Add Delivery Man", 'code' => 0, 'errors' => []], 500);
             } else
-                return response()->json(["message" => "Error on Add Delivery Man", 'code' => 0, 'errors' => []], 500);
-        } else
-            return response()->json($res->message);
+                return response()->json($res->message);
+        });
+
     }
     public function refreshToken(Request $request)
     {
