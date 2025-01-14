ae4fae291bd266da730964e06183fc27e8fc572a
musta <mustafa774519161@gmail.com>
2025-01-14 21:36:11 +0300

    dfdfrfef

--- app/Models/OrdersDelivery.php
+++ app/Models/OrdersDelivery.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 class OrdersDelivery
 {
-   public static $tableName = "deliveryMen";
+   public static $tableName = "ordersDelivery";
    public static $id = "id";
    public static $orderId = "orderId";
    public static $locationId = "locationId";
