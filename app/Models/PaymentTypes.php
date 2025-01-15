3348c5506eba8f5aba9337b5258271c6240eb46d
musta <mustafa774519161@gmail.com>
2025-01-15 21:14:11 +0300

    dfe

--- app/Models/PaymentTypes.php
+++ app/Models/PaymentTypes.php
@@ -6,8 +6,8 @@ class PaymentTypes
 {
    public static $tableName = "paymentTypes";
    public static $id = "id";
-   public static $name = "storeId";
-   public static $image = "userId";
+   public static $name = "name";
+   public static $image = "image";
    public static $createdAt = "createdAt";
    public static $updatedAt = "updatedAt";
 }
