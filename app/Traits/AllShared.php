ceed8cd6a0fae07cdf456cd025ccc7cd24bf4ac0
musta <mustafa774519161@gmail.com>
2025-01-21 08:29:08 +0300

    sff

--- app/Traits/AllShared.php
+++ app/Traits/AllShared.php
@@ -392,7 +392,7 @@ trait AllShared
                     ->first();
 
                 $productIds = json_decode($storeConfig->products);
-                // print_r($productIds);fe
+                print_r($productIds);
     
                 return $query->where(StoreProducts::$tableName . '.' . StoreProducts::$storeId, '=', $storeConfig->storeIdReference)
                     ->whereNotIn(StoreProducts::$tableName . '.' . StoreProducts::$id, $productIds);
