629d5bcc847c514635409399072a2fe7fb25775a
musta <mustafa774519161@gmail.com>
2025-01-31 19:34:27 +0300

    sdfe

--- app/Models/StoreSubscriptions.php
+++ app/Models/StoreSubscriptions.php
@@ -7,7 +7,7 @@ class StoreSubscriptions
 {
    public static $tableName = "storeSubscriptions";
    public static $id = "id";
-   public static $isPremuim = "isPremuim";
+   public static $isPremium = "isPremium";
    public static $points = "points";
    public static $storeId = "storeId";
    public static $expireAt = "expireAt";
