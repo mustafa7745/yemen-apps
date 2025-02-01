66e2099e2e967516422cb52aa35b4bb354cc5242
musta <mustafa774519161@gmail.com>
2025-02-01 20:26:35 +0300

    sde

--- app/Traits/AllShared.php
+++ app/Traits/AllShared.php
@@ -1680,7 +1680,7 @@ trait AllShared
 
         $failProcesses = DB::table(table: FailProcesses::$tableName)
             ->where(FailProcesses::$tableName . '.' . FailProcesses::$deviceId, '=', $deviceId)
-            // ->whereBetween(FailProcesses::$tableName . '.' . FailProcesses::$createdAt, [now()->subMinutes(value: 1), now()])
+            ->whereBetween(FailProcesses::$tableName . '.' . FailProcesses::$createdAt, [now()->subMinutes(1), now()])
             ->when($userId != null, function ($query) use ($userId) {
                 return $query->where(FailProcesses::$tableName . '.' . FailProcesses::$userId, '=', $userId);
             })
