fb6d0072fa9cb6b73a9a5f18ebf1360b9b8c8f66
musta <mustafa774519161@gmail.com>
2025-02-05 00:03:30 +0300

    يسي

--- app/Http/Controllers/Api/StoreManager/StoreManagerControllerUpdate.php
+++ app/Http/Controllers/Api/StoreManager/StoreManagerControllerUpdate.php
@@ -167,9 +167,9 @@ class StoreManagerControllerUpdate extends Controller
     }
     public function updateProductName(Request $request)
     {
-        $myData = $this->getMyData($request, true);
-        // $accessToken = $myData['accessToken'];
-        $store = $myData['app'];
+        // $myData = $this->getMyData($request, true);
+        // // $accessToken = $myData['accessToken'];
+        // $store = $myData['app'];
 
         $this->validRequestV1($request, [
             'productId' => 'required|string|max:11',
