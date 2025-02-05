06b8bdbb4cec314650bedc1f5a4f0fbe2c9732eb
musta <mustafa774519161@gmail.com>
2025-02-05 17:45:09 +0300

    dggr

--- app/Http/Controllers/Api/Users/UserControllerGet.php
+++ app/Http/Controllers/Api/Users/UserControllerGet.php
@@ -29,7 +29,7 @@ class UserControllerGet extends Controller
 
     public function login(Request $request)
     {
-        $myData = $this->getMyData($request, null, false);
+        $myData = $this->getMyData($request, null, false, false, 'login');
         $app = $myData['app'];
         return (new LoginController($app->id))->login($request);
     }
