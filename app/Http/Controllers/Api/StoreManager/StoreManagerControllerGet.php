e84795d5a937fe3f7040b5eceee413861ba1890b
musta <mustafa774519161@gmail.com>
2025-02-07 17:57:04 +0300

    sdfe

--- app/Http/Controllers/Api/StoreManager/StoreManagerControllerGet.php
+++ app/Http/Controllers/Api/StoreManager/StoreManagerControllerGet.php
@@ -790,7 +790,7 @@ class StoreManagerControllerGet extends Controller
 
         $this->validRequestV1($request, [
             'from' => 'required|string|max:5',
-            'fromDate' => 'required|date_format:Y-m-d',
+            'fromDate' => 'required|date_format:d-m-Y',
             'toDate' => 'required|date_format:Y-m-d',
         ]);
         $from = $request->input('from');
