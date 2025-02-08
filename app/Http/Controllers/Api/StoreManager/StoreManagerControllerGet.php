9cd85c4d78907bc7bd253f8644e8f77c2d973fab
musta <mustafa774519161@gmail.com>
2025-02-08 11:22:09 +0300

    sdfe

--- app/Http/Controllers/Api/StoreManager/StoreManagerControllerGet.php
+++ app/Http/Controllers/Api/StoreManager/StoreManagerControllerGet.php
@@ -832,11 +832,11 @@ class StoreManagerControllerGet extends Controller
         $fromDate = $request->input('fromDate');
         $toDate = $request->input('toDate');
         //
-        $fromDate = Carbon::parse($fromDate)->startOfDay(); // Parse fromDate to Carbon instance
-        $toDate = Carbon::parse($toDate)->endOfDay(); // Set toDate to end of the day (23:59:59)
+        // $fromDate = Carbon::parse($fromDate)->startOfDay(); // Parse fromDate to Carbon instance
+        // $toDate = Carbon::parse($toDate)->endOfDay(); // Set toDate to end of the day (23:59:59)
 
-        print_r($fromDate);
-        print_r($toDate);
+        // print_r($fromDate->dat);
+        // print_r($toDate);
 
 
         // $to = $request->input('to');
