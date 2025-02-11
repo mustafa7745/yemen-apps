c37e1abeb2f09e4fecf556513f6e2b55f2a3870e
musta <mustafa774519161@gmail.com>
2025-02-11 17:32:36 +0300

    sdfe

--- app/Http/Controllers/Api/StoreManager/StoreManagerControllerGet.php
+++ app/Http/Controllers/Api/StoreManager/StoreManagerControllerGet.php
@@ -316,7 +316,7 @@ class StoreManagerControllerGet extends Controller
         // // print_r($request->all());
         // $accessToken = (new LoginController($this->appId))->readAccessToken($request);
 
-        $myData = $this->getMyData(request: $request, appId: $this->appId, withStore: true, storePoints: 2);
+        $myData = $this->getMyData(request: $request, appId: $this->appId);
         $accessToken = $myData['accessToken'];
 
         $data = DB::table(Stores::$tableName)
