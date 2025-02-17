15c0c436c2258f28f4850ee98fdd1560510e3877
musta <mustafa774519161@gmail.com>
2025-02-17 08:25:37 +0300

    sdfe

--- app/Http/Controllers/Api/StoreManager/StoreManagerControllerUpdate.php
+++ app/Http/Controllers/Api/StoreManager/StoreManagerControllerUpdate.php
@@ -727,7 +727,7 @@ class StoreManagerControllerUpdate extends Controller
         // ;
 
         $purchase = $service->purchases_products->get('com.fekraplatform.storemanger', 'point5', $purchaseToken);
-        print_r($purchase);
+        print_r($purchase->getSku());
         $productIds = $request->input('productId');
         // print_r($productIds[0]);
         $productIds = json_decode($productIds);
