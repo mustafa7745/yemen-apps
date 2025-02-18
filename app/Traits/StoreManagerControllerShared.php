3d6386da1e781a30c8093a5b5f98e387583ed943
musta <mustafa774519161@gmail.com>
2025-02-18 07:59:31 +0300

    sfgr

--- app/Traits/StoreManagerControllerShared.php
+++ app/Traits/StoreManagerControllerShared.php
@@ -32,6 +32,7 @@ trait StoreManagerControllerShared
             $service = $this->getServiceClient();
             $purchase = $service->purchases_products->get($app->packageName, $inAppProduct->productId, $purchaseToken);
             print_r($purchase);
+            print_r('purchaseSatae' . ': ' . $purchase->purchaseState);
             // print_r($purchaseToken);
             // print_r($googlePurchase->productId);
             // print_r($app->packageName);
