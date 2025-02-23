00b664bf7943421fefd02ff39f077dd8d313c2ef
musta <mustafa774519161@gmail.com>
2025-02-23 20:31:49 +0300

    sdfe

--- app/Traits/StoreManagerControllerShared.php
+++ app/Traits/StoreManagerControllerShared.php
@@ -182,7 +182,7 @@ trait StoreManagerControllerShared
         $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc')); // Initialization Vector
 
         // 2. Encrypt the data
-        $dataToEncrypt = $data;
+        $dataToEncrypt = mb_convert_encoding($data, 'UTF-8');
         $ciphertext = openssl_encrypt($dataToEncrypt, 'aes-256-cbc', $key, 0, $iv);
 
         // Encode the ciphertext and IV so they can be stored or transmitted (Base64 encoding)
@@ -228,7 +228,7 @@ trait StoreManagerControllerShared
         $decryptedData = openssl_decrypt($decodedCiphertext, 'aes-256-cbc', $key, 0, $decodedIv);
 
         // echo "Decrypted data: " . $decryptedData . "\n";
-        return $decryptedData;
+        return mb_convert_encoding($decryptedData, 'UTF-8');
     }
     function isValidJson($string)
     {
