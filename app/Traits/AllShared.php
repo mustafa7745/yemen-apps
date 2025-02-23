b2e67c6fe8fc2ab43cf3e7c55543ea652a816cb9
musta <mustafa774519161@gmail.com>
2025-02-23 17:27:36 +0300

    sdfe

--- app/Traits/AllShared.php
+++ app/Traits/AllShared.php
@@ -1743,8 +1743,8 @@ trait AllShared
                     return response()->json(['success' => true]);
                 }
   
-                // $whatsapp->sendMessageText($phoneNumber, json_encode($app) . " ID");
-                
+                $whatsapp->sendMessageText($phoneNumber, json_encode($app) . " ID");
+
                 DB::table(table: Apps::$tableName)
                     ->where(Apps::$id, '=', $app->id)
                     ->update(
