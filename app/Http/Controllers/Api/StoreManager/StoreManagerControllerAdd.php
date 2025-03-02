4acf542b77d935d028b3caf7466c209e2e210359
musta <mustafa774519161@gmail.com>
2025-03-02 23:20:43 +0300

    sdfe

--- app/Http/Controllers/Api/StoreManager/StoreManagerControllerAdd.php
+++ app/Http/Controllers/Api/StoreManager/StoreManagerControllerAdd.php
@@ -426,6 +426,9 @@ class StoreManagerControllerAdd extends Controller
             'name' => 'required|string|max:100',
             'typeId' => 'required|string|max:1',
             'cover' => 'required|image|max:100',
+            'latitude' => 'required|image|max:100',
+            'longitude' => 'required|image|max:100',
+
         ]);
 
         // $validator = Validator::make($request->all(), [
