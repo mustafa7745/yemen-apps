8026b5f897d395c7b254e79e1f07df64b9108b06
musta <mustafa774519161@gmail.com>
2025-03-03 16:45:06 +0300

    adfe

--- app/Http/Controllers/Api/Stores/StoresControllerGet.php
+++ app/Http/Controllers/Api/Stores/StoresControllerGet.php
@@ -43,7 +43,7 @@ class StoresControllerGet extends Controller
         $longitude = $request->input('longitude');
 
         $isLocationNull = is_null($latitude) || is_null($longitude);
-        print_r($isLocationNull);
+        print_r($isLocationNull ? "null":"not null");
 
         $stores = DB::table(Stores::$tableName)
             ->where(Stores::$tableName . '.' . Stores::$mainCategoryId, '=', $mainCategoryId)
