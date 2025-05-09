f7bedc614102bc81c4089cc385929bf1399f57c2
musta <mustafa774519161@gmail.com>
2025-05-09 22:18:37 +0300

    vt

--- app/Http/Controllers/Api/StoreManager/StoreManagerControllerUpdate.php
+++ app/Http/Controllers/Api/StoreManager/StoreManagerControllerUpdate.php
@@ -1018,9 +1018,9 @@ class StoreManagerControllerUpdate extends Controller
         if ($app->password == null) {
             throw new CustomException("يتم اضافة رمز التحقق للتطبيق بعد", 0, 403);
         }
-        if (Hash::check($passwordService, $app->password) == false) {
-            throw new CustomException("رمز غير صحيح", 0, 403);
-        }
+        // if (Hash::check($passwordService, $app->password) == false) {
+        //     throw new CustomException("رمز غير صحيح", 0, 403);
+        // }
         if ($this->isValidJson($jsonContent) == false) {
             return $this->responseError2(" الملف  خاطئ", [], 0, 405);
         }
